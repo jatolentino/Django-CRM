bb8960067922131ed0bc86732008ff91de2dd452
Jose A. Tolentino <61167951+jatolentino@users.noreply.github.com>
2022-06-07 18:45:19 -0500

    Add files via upload

--- leads/urls.py
+++ leads/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from .views import (LeadListView, LeadDetailView, LeadCreateView, LeadDeleteView, 
-LeadUpdateView)
+from .views import LeadListView, LeadDetailView, LeadCreateView, LeadDeleteView, LeadUpdateView
 #from .views import (lead_list, lead_detail, lead_create, lead_update,
 #lead_delete, LeadListView, LeadDetailView, LeadCreateView, LeadDeleteView, 
 #LeadUpdateView)
@@ -15,9 +14,4 @@ urlpatterns = [
     path('create/', LeadCreateView.as_view(), name='lead-create'),
     path('<int:pk>/update/', LeadUpdateView.as_view(), name='lead-update'),
     path('<int:pk>/delete/', LeadDeleteView.as_view(), name='lead-delete'),
-    #path('', lead_list, name='lead-list'),
-    #path('<int:pk>/', lead_detail, name='lead-detail'),
-    #path('<int:pk>/update/', lead_update, name='lead-update'),
-    #path('<int:pk>/delete/', lead_delete, name='lead-delete'),
-    #path('create/', lead_create, name='lead-create')
 ]
